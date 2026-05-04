ceedce21292c82194d7c0f57eb9f254436139132
Nikhilesh Ravulapelli <sumu8224@gmail.com>
2026-05-04 11:54:44 +0530

    feat: initialised fastapi with basic functionalities

---
+++ db/session.py
@@ -0,0 +1,17 @@
+# Here I want to store the credentials for the database connection which is azure sql database. I will use the pyodbc library to connect to the database and execute queries.
+import pyodbc
+
+def get_db_connection():
+    try:
+        server = 'azure-learn-db.database.windows.net'
+        database = 'azure-learn-db'
+        username = 'CloudSAf8467395'
+        password = 'haisiri@123456'
+        connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+        conn = pyodbc.connect(connection_string)
+        return conn
+    except Exception as e:
+        print(f"Error connecting to database: {e}")
+        raise e
+
+    
---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from route.todo_v1 import router
+
+
+app = FastAPI()
+
+app.include_router(router, prefix="/v1/todos", tags=["todo"])
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
