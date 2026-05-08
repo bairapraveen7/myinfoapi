b3ea9df5f9ad92163db65d4d82289ed9f8842eba
bairapraveen7 <bairapraveen8@gmail.com>
2026-05-08 14:52:28 +0530

    fix: removed hardcoded secrets and rotated them in azure

--- db/session.py
+++ db/session.py
@@ -1,12 +1,13 @@
 # Here I want to store the credentials for the database connection which is azure sql database. I will use the pyodbc library to connect to the database and execute queries.
 import pyodbc
+import os
 
 def get_db_connection():
     try:
-        server = 'azure-learn-db.database.windows.net'
-        database = 'profile'
-        username = 'CloudSAf8467395'
-        password = 'haisiri@123456'
+        server = os.getenv("SQL_SERVER")
+        database = os.getenv("SQL_DATABASE_NAME")
+        username = os.getenv("SQL_USERNAME")
+        password = os.getenv("SQL_PASWORD")
         connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
         conn = pyodbc.connect(connection_string)
         return conn
