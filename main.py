4aa1129ffd4ba69e5faf9f53120a82860a7869f1
bairapraveen7 <bairapraveen8@gmail.com>
2026-05-08 15:23:05 +0530

    fix: added load_dotenv, and changed spelling of password

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from route.profile_v1 import router
 
+# load dotenv variables
+load_dotenv()
 
 app = FastAPI()
+
 
 # app.include_router(router, prefix="/v1/todos", tags=["todo"])
 app.include_router(router, prefix="/v1/profile", tags=["profile"])
